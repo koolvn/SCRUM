266c43b9b88b8fde77d89baccf4d26a00bea403b
Vladimir Kulyashov <kulyashovv@gmail.com>
2020-06-24 03:24:15 +0500

    Прописал TODO, добавил отображение игрового поля, добавил цвета в текст, теперь игра не вылетает, при неправильном вводе координат

--- torpydo/battleship.py
+++ torpydo/battleship.py
@@ -1,16 +1,16 @@
 import random
-
 import colorama
 from colorama import Fore, Back, Style
-
 from torpydo.ship import Color, Letter, Position, Ship
 from torpydo.game_controller import GameController
 
 myFleet = []
 enemyFleet = []
+player = None
+
 
 def main():
-    colorama.init()
+    colorama.init(convert=True)
     print(Fore.YELLOW + r"""
                                     |__
                                     |\/
@@ -27,48 +27,81 @@ def main():
  \_________________________________________________________________________|""" + Style.RESET_ALL)
 
     initialize_game()
-
     start_game()
+
+
+def show_rules():
+    # TODO: написать правила игры
+    print('''
+    ''')
+
+
+def show_grid(shots_and_pos):
+    # TODO: Переделать в класс. Нужно запоминать и отрисовывать выстрелы и расстановку кораблей.
+    print('''
+       |   |   |   |   |   |   |   |   | 
+    ___|_A_|_B_|_C_|_D_|_E_|_F_|_G_|_H_|___
+    _1_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _2_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _3_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _4_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _5_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _6_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _7_|{}|{}|{}|{}|{}|{}|{}|{}|___
+    _8_|{}|{}|{}|{}|{}|{}|{}|{}|___
+       |   |   |   |   |   |   |   |   |
+    '''.format(*shots_and_pos))
+
 
 def start_game():
     global myFleet, enemyFleet
 
     print(r'''
-                  __
-                 /  \
-           .-.  |    |
-   *    _.-'  \  \__/
-    \.-'       \
-   /          _/
+    #### WARSHIPS ####
+    ##### SCRUM #####
+    #### BATTLE #####
+        *         __
+      *    *     /|| \
+           .-.  |  o |
+       _.-'  \  | Oo |
+    .-'       \  \__/
+   /  SUSHA   _/
    |      _  /
    |     /_\
     \    \_/
-     """"""""''')
+     """"""""
+     ________________
+     ver. 0.1
+     ''')
 
     while True:
-        print()
-        print("Player, it's your turn")
+        # TODO: Избавится от бесконечного цикла
+        print("It's your turn,", Fore.CYAN + f"{player}" + Style.RESET_ALL)
+        show_grid(
+            [Fore.GREEN + '_X_' + Style.RESET_ALL if (x // 2 == 0) else Fore.RED + '_X_' + Style.RESET_ALL for x in
+             range(100, 164)])
         position = parse_position(input("Enter coordinates for your shot :"))
         is_hit = GameController.check_is_hit(enemyFleet, position)
         if is_hit:
-            print(r'''
+            print(Fore.LIGHTRED_EX + r'''
                 \          .  ./
-              \   .:"";'.:..""   /
+              \   .:"";'.:..""   /d
                  (M^^.^~~:.'"").
             -   (/  .    . . \ \)  -
                ((| :. ~ ^  :. .|))
             -   (\- |  \ /  |  /)  -
                  -\  \     /  /-
-                   \  \   /  /''')
+                   \  \   /  /''' + Style.RESET_ALL)
 
-        print("Yeah ! Nice hit !" if is_hit else "Miss")
+        print("Yeah ! Nice hit !" if is_hit else Fore.LIGHTBLACK_EX + "Miss" + Style.RESET_ALL)
 
         position = get_random_position()
         is_hit = GameController.check_is_hit(myFleet, position)
         print()
-        print(f"Computer shoot in {position.column.name}{position.row} and {'hit your ship!' if is_hit else 'miss'}")
+        print(
+            f"Computer shoot in {position.column.name}{position.row} and {'hit your ship!' if is_hit else Fore.LIGHTBLUE_EX + 'miss' + Style.RESET_ALL}")
         if is_hit:
-            print(r'''
+            print(Fore.MAGENTA + r'''
                 \          .  ./
               \   .:"";'.:..""   /
                  (M^^.^~~:.'"").
@@ -76,16 +109,26 @@ def start_game():
                ((| :. ~ ^  :. .|))
             -   (\- |  \ /  |  /)  -
                  -\  \     /  /-
-                   \  \   /  /''')
+                   \  \   /  /''' + Style.RESET_ALL)
 
-def parse_position(input: str):
-    letter = Letter[input.upper()[:1]]
-    number = int(input[1:])
+
+def parse_position(position: str):
+    """ Проверяет позицию """
+    # TODO: Нельзя стрелять дважды в одну и ту же клетку
+    # TODO: Показывать выбранную клетку на карте
+    if len(position) != 2 or not position[0].isalpha() or not position[1].isdigit():
+        print('Sorry but you have to input only two symbols,'
+              ' so the position should contain column LETTER and row NUMBER. E.g. A5\n')
+        position = parse_position(input('Please try again:\n'))
+    letter = Letter[position.upper()[:1]]
+    number = int(position[1:])
     position = Position(letter, number)
 
-    return Position(letter, number)
+    return position
+
 
 def get_random_position():
+    # TODO: Docstring
     rows = 8
     lines = 8
 
@@ -95,28 +138,42 @@ def get_random_position():
 
     return position
 
+
 def initialize_game():
+    # TODO: Докстринг
+    global player
+    player = input('Enter your name: ')
     initialize_myFleet()
 
     initialize_enemyFleet()
 
+
 def initialize_myFleet():
+    # TODO: Докстринг
+    # TODO: Корабли должны выставлятся согласно правил игры
     global myFleet
 
     myFleet = GameController.initialize_ships()
+    show_grid([Back.BLUE + Fore.BLACK + '___' + Style.RESET_ALL for x in range(64)])
+    show_grid([Fore.GREEN + '_X_' + Style.RESET_ALL if (x % 2 == 0) else Fore.RED + '_X_' + Style.RESET_ALL for x in
+               range(100, 164)])
 
     print("Please position your fleet (Game board has size from A to H and 1 to 8) :")
 
     for ship in myFleet:
         print()
-        print(f"Please enter the positions for the {ship.name} (size: {ship.size})")
+        print(Fore.BLUE + f"Please enter the positions for the {ship.name} (size: {ship.size})" + Style.RESET_ALL)
 
         for i in range(ship.size):
             position_input = input(f"Enter position {i} of {ship.size} (i.e A3):")
 
             ship.add_position(position_input)
+    print([_ship.positions for i, _ship in enumerate(myFleet)])
+
 
 def initialize_enemyFleet():
+    # TODO: Корабли противника должны выставлятся случайным образом
+    # TODO: Докстринг
     global enemyFleet
 
     enemyFleet = GameController.initialize_ships()
@@ -142,6 +199,8 @@ def initialize_enemyFleet():
 
     enemyFleet[4].positions.append(Position(Letter.C, 5))
     enemyFleet[4].positions.append(Position(Letter.C, 6))
+    print('DEBUG:', [(_ship.name, _ship.positions) for _ship in enemyFleet])
+
 
 if __name__ == '__main__':
     main()
--- torpydo/ship.py
+++ torpydo/ship.py
@@ -1,11 +1,14 @@
 from enum import Enum
 
+
 class Color(Enum):
+    # TODO: Применить эти цвета
     CADET_BLUE = 1
     CHARTREUSE = 2
     ORANGE = 3
     RED = 4
     YELLOW = 5
+
 
 class Letter(Enum):
     A = 1
@@ -17,18 +20,24 @@ class Letter(Enum):
     G = 7
     H = 8
 
+
 class Position(object):
     def __init__(self, column: Letter, row: int):
         self.column = column
         self.row = row
 
     def __eq__(self, other):
+        """ Проверяет позицию на равенство other
+        :param other: другая позиция
+        :return: True/False
+        """
         return self.__dict__ == other.__dict__
 
     def __str__(self):
         return f"{self.column.name}{self.row}"
 
     __repr__ = __str__
+
 
 class Ship(object):
     def __init__(self, name: str, size: int, color: Color):
@@ -37,12 +46,24 @@ class Ship(object):
         self.color = color
         self.positions = []
 
-    def add_position(self, input: str):
-        letter = Letter[input.upper()[:1]]
-        number = int(input[1:])
-        position = Position(letter, number)
+    def add_position(self, position: str):
+        # TODO: Корабль должен выставлятся согласно правил игры
+        while True:
+            if position.upper()[0] not in Letter._member_names_:
+                print('Sorry this LETTER is not available in this game.\n')
+                position = input('Please try again:\n')
 
+            elif len(position) != 2 or not position[0].isalpha() or not position[1].isdigit():
+                print('Sorry but you have to input only two symbols,'
+                      ' so the position should contain column LETTER and row NUMBER. E.g. A5\n')
+                position = input('Please try again:\n')
+            else:
+                break
+
+        letter = Letter[position.upper()[0]]
+        number = int(position[1])
         self.positions.append(Position(letter, number))
+        return position
 
     def __str__(self):
         return f"{self.color.name} {self.name} ({self.size}): {self.positions}"
